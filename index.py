00199189afba7f1bec9a221e8bffc871b7fc1714
Ramesha55cv <111272582+Ramesha55cv@users.noreply.github.com>
2022-08-27 16:42:59 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'project1',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
